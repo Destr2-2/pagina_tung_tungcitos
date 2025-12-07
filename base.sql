967d649d31a13eaebbf48af9317019744be4b32c
Destr2-2 <modv081214hgtrmca5@cbtis217.edu.mx>
2025-12-06 22:42:49 -0600

    base_usuarios

--- base.sql
+++ base.sql
@@ -3,10 +3,12 @@ use tungtung;
 
 create table usuarios(
 id_usuario int auto_increment primary key,
+email varchar(100) not null,
+usuario varchar(30) not null,
 nombre varchar(40) not null,
 apellidos varchar(40) not null,
-usuario varchar(30) not null,
+f_nacimiento date not null,
 contra varchar(20) not null,
-f_nacimiento date not null
+num_telefono varchar(12) not null
 );
 
